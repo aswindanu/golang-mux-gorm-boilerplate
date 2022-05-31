177628736be0df4fd2a359f5f95e5158b4df2c44
aswindanu <aswindanu.prihastomo@gmail.com>
2022-06-01 01:00:34 +0700

    update note

--- app/model/model.go
+++ app/model/model.go
@@ -26,7 +26,7 @@ func (p *Project) Restore() {
 type Task struct {
 	gorm.Model
 	Title string `json:"title"`
-	// Priority  string     `gorm:"type:ENUM('0', '1', '2', '3');default:'0'" json:"priority"` //FIXME
+	// Priority  string     `gorm:"type:ENUM('0', '1', '2', '3');default:'0'" json:"priority"` //FIXME, see this https://stackoverflow.com/a/67743613
 	Deadline  *time.Time `gorm:"default:null" json:"deadline"`
 	Done      bool       `json:"done"`
 	ProjectID uint       `json:"project_id"`
