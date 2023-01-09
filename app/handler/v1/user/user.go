ec92e9d54f1666ef0107717e89af2af8f4e01948
aswindanu <aswindanu.prihastomo@gmail.com>
2023-01-09 11:26:30 +0700

    Update using service module

--- app/handler/v1/user/user.go
+++ app/handler/v1/user/user.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 	"strconv"
 
-	"golang-mux-gorm-boilerplate/app/handler"
+	"golang-mux-gorm-boilerplate/app/service"
 
 	"golang-mux-gorm-boilerplate/app/model"
 
@@ -13,86 +13,79 @@ import (
 	"github.com/jinzhu/gorm"
 )
 
+const MODEL = "User"
+
 func GetAllUsers(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
-	users := []model.User{}
-	db.Find(&users)
-	handler.RespondJSON(w, http.StatusOK, users)
-}
-
-func CreateUser(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
-	user := model.User{}
-
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&user); err != nil {
-		handler.RespondError(w, http.StatusBadRequest, err.Error())
+	users := service.GetAll(db, w, r, MODEL)
+	if users == nil {
 		return
 	}
-	defer r.Body.Close()
 
-	if err := db.Save(&user).Error; err != nil {
-		handler.RespondError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	handler.RespondJSON(w, http.StatusCreated, user)
+	service.RespondJSON(w, http.StatusOK, users)
 }
 
 func GetUser(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
 	userId, _ := strconv.Atoi(vars["id"])
-	user := getUserOr404(db, userId, w, r)
+	user := service.GetByIdOr404(db, w, r, userId, MODEL)
 	if user == nil {
 		return
 	}
 
-	handler.RespondJSON(w, http.StatusOK, user)
+	service.RespondJSON(w, http.StatusOK, user)
+}
+
+func CreateUser(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
+	user := &model.User{}
+
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(user); err != nil {
+		service.RespondError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	defer r.Body.Close()
+
+	user.ValidateData(w, r)
+	if err := service.CreateUpdateOr404(db, w, r, user); err != nil {
+		service.RespondError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	service.RespondJSON(w, http.StatusCreated, user)
 }
 
 func UpdateUser(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
 	userId, _ := strconv.Atoi(vars["id"])
-	user := getUserOr404(db, userId, w, r)
+	user := service.GetByIdOr404(db, w, r, userId, MODEL)
 	if user == nil {
 		return
 	}
 
 	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(&user); err != nil {
-		handler.RespondError(w, http.StatusBadRequest, err.Error())
+	if err := decoder.Decode(user); err != nil {
+		service.RespondError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 	defer r.Body.Close()
 
-	if err := db.Save(&user).Error; err != nil {
-		handler.RespondError(w, http.StatusInternalServerError, err.Error())
+	if err := service.CreateUpdateOr404(db, w, r, *user); err != nil {
+		service.RespondError(w, http.StatusInternalServerError, err.Error())
 		return
 	}
-	handler.RespondJSON(w, http.StatusOK, user)
+	service.RespondJSON(w, http.StatusOK, user)
 }
 
 func DeleteUser(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 
 	userId, _ := strconv.Atoi(vars["id"])
-	user := getUserOr404(db, userId, w, r)
+	user := service.GetByIdOr404(db, w, r, userId, MODEL)
 	if user == nil {
 		return
 	}
 
-	if err := db.Delete(&user).Error; err != nil {
-		handler.RespondError(w, http.StatusInternalServerError, err.Error())
-		return
-	}
-	handler.RespondJSON(w, http.StatusNoContent, nil)
-}
-
-// getUserOr404 gets a user instance if exists, or respond the 404 error otherwise
-func getUserOr404(db *gorm.DB, id int, w http.ResponseWriter, r *http.Request) *model.User {
-	user := model.User{}
-	if err := db.First(&user, id).Error; err != nil {
-		handler.RespondError(w, http.StatusNotFound, err.Error())
-		return nil
-	}
-	return &user
+	service.DeleteOr404(db, w, r, *user)
+	service.RespondJSON(w, http.StatusNoContent, nil)
 }
